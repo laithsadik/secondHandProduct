3e89af38ca59c4c994c60b801b221a4769766111
laithsadik <101054560+laithsadik@users.noreply.github.com>
2024-04-16 10:58:01 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
-import ListingItem from "../components/listingItem";
+import ListingItem from "../components/ListingItem";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
